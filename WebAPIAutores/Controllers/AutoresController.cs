4a0525d4b3cfc34d92d3e55c6bc65da8aa44ae85
Alexis Alcantara <alexisv.alcantara@gmail.com>
2022-01-12 23:09:05 -0600

    Agregar archivos de proyecto.

---
+++ WebAPIAutores/Controllers/AutoresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { Id = 1, Nombre = "Alexis"},
+                new Autor() { Id = 2, Nombre = "Sandra"}
+            };
+        }
+    }
+}
