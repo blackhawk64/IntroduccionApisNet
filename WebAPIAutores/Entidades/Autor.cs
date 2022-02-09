3605ef0e538e6e31ab2ecf1ac0591419367a3083
Alexis Alcantara <alexisv.alcantara@gmail.com>
2022-02-09 02:10:39 -0600

    Ejemplos de validaciones por defecto. Introduccion al decorador NotMapped para incluir un atributo que no tenga que estar relacionado con la columna de una tabla

--- WebAPIAutores/Entidades/Autor.cs
+++ WebAPIAutores/Entidades/Autor.cs
@@ -1,9 +1,23 @@
-﻿namespace WebAPIAutores.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIAutores.Entidades
 {
     public class Autor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido*")]
+        [StringLength(maximumLength: 10, ErrorMessage = "El campo {0} no acepta mas de {1} caracteres")]
         public string Nombre { get; set; }
+        [Range(18,90)]
+        [NotMapped]
+        public int Edad { get; set; }
+        [CreditCard]
+        [NotMapped]
+        public string TarjetaCredito { get; set; }
+        [Url]
+        [NotMapped]
+        public string UrlAutor { get; set; }
         public List<Libro> Libros { get; set; }
     }
 }
